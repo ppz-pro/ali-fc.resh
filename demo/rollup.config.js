a4fd13648ab64f72979f4b97cc8de5f683a3562d
关于 <372301467@qq.com>
2021-11-23 17:07:00 +0800

    demo 添加 rollup 支持 更新依赖

---
+++ demo/rollup.config.js
@@ -0,0 +1,19 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    exports: 'auto'
+  },
+  plugins: [
+    nodeResolve({
+      preferBuiltins: true
+    }),
+    commonjs(),
+    json()
+  ]
+}
